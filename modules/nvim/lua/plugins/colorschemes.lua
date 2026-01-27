c4164468b4b32060ec956c3050d69f51935e568b
Andres Hernandez <andres.hernandez@e-space.com>
2026-01-27 11:11:54 -0800

    try to install more colorscheme... currently not working

---
+++ modules/nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    enabled = false,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    lazy = true,
+  },
+  {
+    "neanias/everforest.nvim",
+    name = "everforest",
+    lazy = true,
+  },
+  {
+    "Edeneast/nightfox.nvim",
+    name = "nightfox",
+    lazy = true,
+  },
+}
