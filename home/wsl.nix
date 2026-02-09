08a1545dc5e2b0d363cf39f9977b6e1b95b492a0
Andres Hernandez <andres.hernandez@e-space.com>
2026-02-09 15:43:31 -0800

    somehow invoking zsh like this broke vscode and cursor devcontainers entirely. patch fix.

--- home/wsl.nix
+++ home/wsl.nix
@@ -12,7 +12,7 @@
   programs.bash = {
     enable = true;
     initExtra = ''
-      if [[ $(ps --no-header --pid=$PPID --format=comm) != "zsh" && -z "$ZSH_VERSION" ]]; then
+      if [[ $(ps --no-header --pid=$PPID --format=comm) != "zsh" && -z "$ZSH_VERSION" && -z "$VSCODE_RESOLVING_ENVIRONMENT" && -t 0 ]]; then
         exec zsh
       fi
     '';
