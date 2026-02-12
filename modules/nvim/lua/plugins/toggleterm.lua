4cf68a9315517542bf5318d4d5ca5e063f533a48
Andres Hernandez <andres.hernandez@e-space.com>
2026-02-12 13:04:41 -0800

    toggleterm usability. careful with using nvim inside this terminal. no config for that yet.

--- modules/nvim/lua/plugins/toggleterm.lua
+++ modules/nvim/lua/plugins/toggleterm.lua
@@ -1,11 +1,14 @@
 return {
   'akinsho/toggleterm.nvim',
   version = "*",
+  keys = {
+    { '<leader>t', '<cmd>ToggleTerm<CR>', desc = 'Open terminal' },
+  },
   opts = {
-    open_mapping = '<leader>t',
     direction = 'float',
     shade_terminals = true,
     start_in_insert = true,
+    dir = vim.fn.getcwd(),
     float_opts = {
       border = 'curved',
       width = function()
@@ -15,5 +18,11 @@ return {
         return math.floor(vim.o.lines * 0.85)
       end,
     },
+    on_open = function(term)
+      -- <Esc> exits terminal mode to normal mode
+      vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { buffer = term.bufnr, desc = 'Exit terminal mode' })
+      -- q in normal mode closes the terminal
+      vim.keymap.set('n', 'q', '<cmd>close<CR>', { buffer = term.bufnr, desc = 'Close terminal' })
+    end,
   },
 }
