f3f19d9f4b6b7c34d81f19a6b95372de2c23b247
Andres Hernandez <andres.hernandez@e-space.com>
2026-02-13 01:06:41 +0000

    osc52 clipboard passthrough testing

---
+++ modules/nvim/lua/plugins/clipboard.lua
@@ -0,0 +1,17 @@
+local ok, osc52 = pcall(require, "vim.ui.clipboard.osc52")
+if ok then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = osc52.copy("+"),
+      ["*"] = osc52.copy("*"),
+    },
+    paste = {
+      ["+"] = osc52.paste("+"),
+      ["*"] = osc52.paste("*"),
+    },
+  }
+
+  -- Make regular yanks use system clipboard
+  vim.opt.clipboard = "unnamedplus"
+end
